728f76486c161250e2ea45ea5cddab74e6e01a1d
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-13 14:10:23 -0700

    my first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -is the entry point for the program.
+ *
+ * Return:zero if no error, Non-zero value if errors.
+ */
+int main(void)
+{
+	printf("size of a char: %id byte(s)\n", sizeof(char));
+	printf("size of an int: %id byte(s)\n", sizeof(int));
+	printf("size of a long int: %id byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %id byte(s)\n", sizeof(long long int));
+	printf("size of a float: %id byte(s)\n", sizeof(float));
+	return (0);
+}

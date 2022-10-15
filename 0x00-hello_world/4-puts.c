e4b9392cca5af1711fdfcc57a4e2393b5c29c116
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-15 09:46:00 -0700

    my first line of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
- * main- serves as the entry point for the program*
- * Return: By default returns zero if no errors otherwise returns non-zero value.
- */
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
+*/
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-		return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,4 @@
+{
 #include <stdio.h>
 /**
  * main-entry point for the program.
@@ -8,4 +9,5 @@ int main (void)
 { 
 	printf("with proper grammar. but the outcome is a piece of art.\n");
 	return (0);
+}
 }

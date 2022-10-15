93d6a1681b271e97bd1415dd389f878fd4fbe244
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-15 10:13:34 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-{
 #include <stdio.h>
 /**
- * main-entry point for the program.
+ * main - print a c program using printf
  *
- * Return:0 if no errors, return non zero if errors
- */
-int main (void)
-{ 
+ * Return: 0
+*/
+int main(void)
+{
 	printf("with proper grammar. but the outcome is a piece of art.\n");
 	return (0);
-}
 }

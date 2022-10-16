e37ed110579ed9d231b48f2144edfb982cdb8b04
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-16 12:34:17 -0700

    last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (ldigit < 6 && ldigit != 0)
 	{
-		printf("Last digit of %d id %d and is less than 6 and not 0\n", n, ldigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldigit);
 	}
 	return (0);
 }

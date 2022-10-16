d687d3ec0e26dfbce284eb59a278306d33758762
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-16 13:11:14 -0700

    6-print_numberz commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * Return:0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'o'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n')
+		return (0);
+}

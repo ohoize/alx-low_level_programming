0ec0947d26a3e04202d363474973e6b9c1a91646
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-25 09:00:43 -0700

    2 commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 	int i:
 
-		printf("Infinite loop incoming :(\n");
+		printf("Infinite loop incoming \n");
 
 	i = 0;
 
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,17 +7,16 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- *
  * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
 {
-	    if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	    if ((year % 400 == 0 || (year % 4 == 0) && year % 100 != 0))
 	    {
-		    if (month > 2 && day >= 60)
+		    if (month >= 3 && day >= 60)
 		    {
-			    day++;
+			    day++
 		    }
 
 		    printf("Day of the year: %d\n", day);
@@ -35,4 +34,4 @@ void print_remaining_days(int month, int day, int year)
 			    printf("Remaining days: %d\n", 365 - day);
 		    }
 	    }
-}
+}   

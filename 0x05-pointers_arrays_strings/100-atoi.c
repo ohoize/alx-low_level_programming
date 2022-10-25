5b785cfc1d30837c2411142558ac51855b5b547a
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-25 08:17:37 -0700

    atoi comment

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -25,8 +25,21 @@ int _atoi(char *s)
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
-			break;
-		f = 0;
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	i++;
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }

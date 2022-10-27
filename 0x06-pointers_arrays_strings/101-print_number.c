89965d2afda801d201731ab2ea0775ed0517604f
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-10-27 10:52:52 -0700

    print number commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_number - print an integer.
+ * print_number - prints an integer.
  * @n: input integer.
  * Return: no return.
  */
@@ -15,7 +15,7 @@ void print_number(int n)
 	}
 	else
 	{
-	m = n;
+		m = n;
 	}
 
 	d = m;

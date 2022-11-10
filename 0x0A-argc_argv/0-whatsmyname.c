e708aba63374064d9055cbd58506eecf71d21746
Emmanuel_obuhoro <emmanuel.obuhoro@gmail.com>
2022-11-10 01:03:12 -0800

    second commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argv[0]);
 	return (0);
 }
